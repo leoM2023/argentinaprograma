642dff24b0dd862fc10787ca53aed6b084530bef
leoM2023 <leandromontero2003@yahoo.com.ar>
2023-06-27 16:08:37 -0700

    Agregando Practico3

---
+++ Practico3/lanzamiento.js
@@ -0,0 +1,12 @@
+let i = 10;
+for ( i; i >= 0; i--){
+    if (i === 10 ){
+        console.log ("Cuenta regresiva 10");
+    }
+    else if (i === 0){
+        console.log ("Lanzamiento!");
+    }
+    else {console.log (i);
+
+    }
+}
